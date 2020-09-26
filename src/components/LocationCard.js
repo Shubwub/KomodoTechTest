ab54b2cc924b3d78ce86606f9e1546bc3e920b64
shubwub <shubwub@gmail.com>
2020-09-26 18:17:42 +0100

    adds opening map app when card is clicked

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,10 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import {Layout, Text, Input, Divider, Card} from '@ui-kitten/components';
 import {LocationText} from './LocationText';
+import {Linking} from 'react-native';
 
 export const LocationCard = ({name, description, latitude, longitude}) => {
+  const scheme = Platform.select({ios: 'maps:0,0?q=', android: 'geo:0,0?q='});
+  const url = Platform.select({
+    ios: `${scheme}${name}@${latitude},${longitude}`,
+    android: `${scheme}${latitude},${longitude}(${name})`,
+  });
+
   return (
-    <Card>
+    <Card onPress={() => Linking.openURL(url)}>
       <Text category="h4">{name}</Text>
       <Text style={{marginBottom: 20}}>{description}</Text>
       <LocationText location={{latitude, longitude}} />
