0ec0db8aa233a253092eadf01a4446127fc5c4f6
shubwub <shubwub@gmail.com>
2020-09-26 18:04:14 +0100

    reduces elements used in locationText

--- src/components/LocationText.js
+++ src/components/LocationText.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import {Layout, Text, Icon} from '@ui-kitten/components';
+import {Text} from '@ui-kitten/components';
 
 export const LocationText = ({location}) => {
-  console.log(location);
   return (
-    <Layout
-      style={{
-        flex: 1,
-      }}>
-      <Icon
-        style={{
-          width: 32,
-          height: 32,
-        }}
-        name="pin-outline"
-      />
+    <>
       <Text>latitude: {location.latitude}</Text>
       <Text>longitude: {location.longitude}</Text>
-    </Layout>
+    </>
   );
 };
