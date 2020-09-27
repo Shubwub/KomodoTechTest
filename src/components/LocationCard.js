f7a109f1268fa08b4213af7dd12daff2faaff3d8
shubwub <shubwub@gmail.com>
2020-09-27 22:49:47 +0100

    adds deletion function for cards

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,9 +1,20 @@
-import React, {useState, useEffect} from 'react';
-import {Layout, Text, Input, Divider, Card} from '@ui-kitten/components';
+import React from 'react';
+import {Layout, Text, Button, Icon, Card} from '@ui-kitten/components';
 import {LocationText} from './LocationText';
 import {Linking} from 'react-native';
 
-export const LocationCard = ({name, description, latitude, longitude}) => {
+const TrashIcon = () => (
+  <Icon style={{width: 20, height: 20}} fill="#8F9BB3" name="trash" />
+);
+
+export const LocationCard = ({
+  name,
+  description,
+  latitude,
+  longitude,
+  index,
+  deleteLocation,
+}) => {
   const scheme = Platform.select({ios: 'maps:0,0?q=', android: 'geo:0,0?q='});
   const url = Platform.select({
     ios: `${scheme}${name}@${latitude},${longitude}`,
@@ -11,8 +22,22 @@ export const LocationCard = ({name, description, latitude, longitude}) => {
   });
 
   return (
-    <Card onPress={() => Linking.openURL(url)}>
-      <Text category="h4">{name}</Text>
+    <Card onPress={() => Linking.openURL(url)} style={{display: 'flex'}}>
+      <Layout
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Text category="h4">{name}</Text>
+        <Button
+          appearance="ghost"
+          status="danger"
+          accessoryLeft={TrashIcon}
+          onPress={() => deleteLocation(index)}
+        />
+      </Layout>
       <Text style={{marginBottom: 20}}>{description}</Text>
       <LocationText location={{latitude, longitude}} />
     </Card>
